96838581be7606cfb2a695fb32c8a1c6e607a144
mikemeding <mikeymeding@gmail.com>
2014-10-04 23:19:28 -0400

    made cool logo and made image dynamic load work

--- RootSite/public_html/lessons/lesson5/js/lesson5.js
+++ RootSite/public_html/lessons/lesson5/js/lesson5.js
@@ -4,33 +4,93 @@
 //Created on: 09/09/2014
 //Course Info: 91.461 GUI programming 1 Jesse M. Heines 
 //
-//This page is designed to show a simple page that includes some 
-//handwritten css with good selectors and a bit of javascript
+//Custom javascript for loading and organizing photos based on JSON data
 
+// GLOBAL VARS
+var imageData;
+
+
+window.onload = preloader;
 
 // displays the page only when all files have been loaded and DOM is complete
 function preloader() {
+	getImageData();
+	// place photos to dom under content
+
+
+	// unhide loaded content and hide loading view
 	document.getElementById("loading").style.display = "none";
 	document.getElementById("content").style.display = "block";
-	getImageData();
 }
-window.onload = preloader;
 
 /*
  * Get image data from imageInfo.json 
  */
 function getImageData() {
-	var story;
 	jQuery.ajax({
 		async: false,
 		dataType: "json",
 		url: "imageInfo.json",
 		success: function(data) {
-			story = data;
+			loadImageData(data.pages);
 		}
 	});
 
-	console.log(story);
+}
+//<div class="row">
+//					<div class="col-lg-6 col-sm-6">
+//						<a href="img/IMG_2.jpg" data-lightbox="collin-car" data-title="a yellow car">
+//							<img class="img-rounded img-responsive" src="img/IMG_2.jpg" alt="Another yellow car">
+//						</a>
+//					</div>
+//					<div class="col-lg-6 col-sm-6">
+//						<a href="img/IMG_3.jpg" data-lightbox="collin-car" data-title="collin-car">
+//							<img class="img-rounded img-responsive" src="img/IMG_3.jpg" alt="Another yellow car">
+//						</a>
+//					</div>
+
+
+function loadImageData(data) {
+	$.each(data, function(index, element) { // load each page
+
+		// image row div
+		var dualImageDiv = document.createElement("div");
+		dualImageDiv.setAttribute("class", "row");
+
+		$.each(element.images, function(imageIndex, imageElement) { // load each image
+
+			// div for dual image display
+			var formatDiv = document.createElement("div");
+			formatDiv.setAttribute("class", "col-lg-6 col-sm-6");
+
+			// link for lightbox stuff 
+			var link = document.createElement("a");
+			link.setAttribute("href", imageElement.fullPath);
+			link.setAttribute("data-lightbox", "link");
+			link.setAttribute("data-title", imageElement.description);
+
+			// image tag with all data
+			var image = document.createElement("img");
+			image.setAttribute("class", "img-rounded img-responsive");
+			image.setAttribute("src", imageElement.fullPath);
+			image.setAttribute("alt", imageElement.description);
+
+			link.appendChild(image); //image is a child of the lightbox link
+			formatDiv.appendChild(link); // single image div
+			$("#content").append(formatDiv);
+
+		});
+
+
+
+
+
+		$("#content").html(link); // add html to page
+
+	});
+
 }
 
-
+function addDualImages(){
+	
+}
