f6270f75f4ab986c292bf043c3facdeebf59df2b
mike meding <mikeymeding@gmail.com>
2014-10-05 03:04:49 -0400

    got both dual and single images working

--- RootSite/public_html/lessons/lesson5/js/lesson5.js
+++ RootSite/public_html/lessons/lesson5/js/lesson5.js
@@ -31,7 +31,7 @@ function getImageData() {
 		async: false,
 		dataType: "json",
 		url: "imageInfo.json",
-		success: function(data) {
+		success: function (data) {
 			loadImageData(data.pages);
 		}
 	});
@@ -51,46 +51,120 @@ function getImageData() {
 
 
 function loadImageData(data) {
-	$.each(data, function(index, element) { // load each page
+	// create elements
+	var singleImageDiv = document.createElement("div");
+	var dualImageDiv = document.createElement("div");
 
-		// image row div
-		var dualImageDiv = document.createElement("div");
-		dualImageDiv.setAttribute("class", "row");
 
-		$.each(element.images, function(imageIndex, imageElement) { // load each image
+	$.each(data, function (index, element) { // load each page
+
+		var i = 0;
+		for (i = 0; i < element.images.length; i++) { // load all corresponding images
+			if (i + 1 !== element.images.length) {
+				loadDualImage(element.images[i], element.images[i + 1]);
+				i++; // add extra as we are loading 2 images
+			} else {
+				loadSingleImage(element.images[i]); // loads last image in set
+			}
+		}
+
+		/*
+		 * Loads full width single image to #content div
+		 * @param {type} image
+		 * @returns {undefined}
+		 */
+		function loadSingleImage(image) {
+			// dual image elements
+			var link = document.createElement("a");
+			var domImage = document.createElement("img");
+			var formatDiv = document.createElement("div");
+
+			// image row div
+			dualImageDiv.setAttribute("class", "row");
 
 			// div for dual image display
-			var formatDiv = document.createElement("div");
-			formatDiv.setAttribute("class", "col-lg-6 col-sm-6");
+			formatDiv.setAttribute("class", "col-lg-12 col-sm-12");
 
+			// load image 1
 			// link for lightbox stuff 
-			var link = document.createElement("a");
-			link.setAttribute("href", imageElement.fullPath);
-			link.setAttribute("data-lightbox", "link");
-			link.setAttribute("data-title", imageElement.description);
+			link.setAttribute("href", image.fullPath);
+			link.setAttribute("data-lightbox", image.setName);
+			link.setAttribute("data-title", image.description);
 
 			// image tag with all data
-			var image = document.createElement("img");
-			image.setAttribute("class", "img-rounded img-responsive");
-			image.setAttribute("src", imageElement.fullPath);
-			image.setAttribute("alt", imageElement.description);
+			domImage.setAttribute("class", "img-rounded img-responsive");
+			domImage.setAttribute("src", image.fullPath);
+			domImage.setAttribute("alt", image.description);
 
-			link.appendChild(image); //image is a child of the lightbox link
-			formatDiv.appendChild(link); // single image div
-			$("#content").append(formatDiv);
+			link.appendChild(domImage); //image is a child of the lightbox link
+			formatDiv.appendChild(link); // full single image div
+			singleImageDiv.appendChild(formatDiv); // dual image block
 
-		});
+			$("#content").append(singleImageDiv);
+		}
+
+		/*
+		 * Loads and appends dual image block to #content div
+		 */
+		function loadDualImage(image1, image2) {
+			// dual image elements
+			var link1 = document.createElement("a");
+			var link2 = document.createElement("a");
+			var domImage1 = document.createElement("img");
+			var domImage2 = document.createElement("img");
+			var formatDiv1 = document.createElement("div");
+			var formatDiv2 = document.createElement("div");
+
+			// image row div
+			dualImageDiv.setAttribute("class", "row");
+
+			// div for dual image display
+			formatDiv1.setAttribute("class", "col-lg-6 col-sm-6");
+			formatDiv2.setAttribute("class", "col-lg-6 col-sm-6");
+
+			// load image 1
+			// link for lightbox stuff 
+			link1.setAttribute("href", image1.fullPath);
+			link1.setAttribute("data-lightbox", image1.setName);
+			link1.setAttribute("data-title", image1.description);
+
+			// image tag with all data
+			domImage1.setAttribute("class", "img-rounded img-responsive");
+			domImage1.setAttribute("src", image1.fullPath);
+			domImage1.setAttribute("alt", image1.description);
+
+			// load image 2
+			// link for lightbox stuff 
+			link2.setAttribute("href", image2.fullPath);
+			link2.setAttribute("data-lightbox", image2.setName);
+			link2.setAttribute("data-title", image2.description);
+
+			// image tag with all data
+			domImage2.setAttribute("class", "img-rounded img-responsive");
+			domImage2.setAttribute("src", image2.fullPath);
+			domImage2.setAttribute("alt", image2.description);
+
+			link1.appendChild(domImage1); //image is a child of the lightbox link
+			link2.appendChild(domImage2);
+			formatDiv1.appendChild(link1); // full single image div
+			formatDiv2.appendChild(link2);
+			dualImageDiv.appendChild(formatDiv1); // dual image block
+			dualImageDiv.appendChild(formatDiv2);
+
+			$("#content").append(dualImageDiv);
+		}
+		function loadQuadImage(image1, image2, image3, image4) {
+
+		}
 
 
 
 
-
-		$("#content").html(link); // add html to page
 
 	});
 
 }
 
-function addDualImages(){
-	
+function addDualImages() {
+
 }
