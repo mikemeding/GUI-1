b638b9cce4252f23b69a86588f17b98136ea47cd
mikemeding <mikeymeding@gmail.com>
2014-10-23 13:09:45 -0400

    moving to gh-pages

--- RootSite/public_html/js/siteRoot.js
+++
@@ -1,9 +0,0 @@
-//Michael Meding
-//mikeymeding@gmail.com (this is my prefered email)
-//Student studying Computer Science at the University of Massachusetts Lowell
-//Created on: 09/09/2014
-//Course Info: 91.461 GUI programming 1 Jesse M. Heines 
-//
-
-
-
