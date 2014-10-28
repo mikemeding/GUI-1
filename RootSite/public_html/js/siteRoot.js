8d4518c2146034f40b8c1ec7c74fb9ed4b55f573
mikemeding <mikeymeding@gmail.com>
2014-10-28 13:57:46 -0400

    one page web app now

--- RootSite/public_html/js/siteRoot.js
+++ RootSite/public_html/js/siteRoot.js
@@ -4,6 +4,34 @@
 //Created on: 09/09/2014
 //Course Info: 91.461 GUI programming 1 Jesse M. Heines 
 //
-
-
+$(document).ready(function() {
+	// trigger an inital click to load home page
+	$('#tabHome').trigger("click");
+});
+$('#tabHome').click(function(e) {
+	e.preventDefault();
+	// dynamically load on click
+	$("#home").load("indexInfo.html", function() {
+		console.log("loaded indexInfo.html");
+		// show tab once loaded
+		$(this).tab('show');
+	});
+});
+$('#tabLessons').click(function(e) {
+	e.preventDefault();
+	$("#lessons").load("lessonsInfo.html", function() {
+		console.log("loaded lessonsInfo.html");
+		// show tab once loaded
+		$(this).tab('show');
+	});
+});
+$('#tabAbout').click(function(e) {
+	e.preventDefault();
+	$("#about").load("aboutInfo.html", function() {
+		console.log("loaded aboutInfo.html");
+		// show tab once loaded
+		$(this).tab('show');
+	});
+	$(this).tab('show');
+});
 
