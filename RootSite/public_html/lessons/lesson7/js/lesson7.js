612bd03be5ddd09347b891bbcc0e2c3d36c063be
mikemeding <mikeymeding@gmail.com>
2014-11-25 01:27:43 -0500

    added jQuery validation with tabs

--- RootSite/public_html/lessons/lesson7/js/lesson7.js
+++ RootSite/public_html/lessons/lesson7/js/lesson7.js
@@ -10,309 +10,278 @@ var endVal1 = 0;
 var startVal2 = 0;
 var endVal2 = 0;
 var totalTabs = 0;
-
-
-$(document).ready(function() {
-	$("#myForm").load("form.html", function() { // load form modal HTML to the page
-		$("#myModal").modal('show'); // display modal from form.html once loaded
-
-		// LISTENERS
-		$("#multForm").submit(function() {		// when the submit button is clicked on the modal
-			$("#myModal").modal('hide'); // attempt to hide which triggers hide event calling validation
-		});
-
-		// when any kind of hide action is preformed on the modal
-//		$('#myModal').on('hidden.bs.modal', function (event) {
-//			if (validation()) { // preform number validation
-//				$("#myModal").modal('hide'); // hide modal completely if validation succedes
-////				var tableHTML = renderTable();
-////				addTab(tableHTML);
-//				AddTabButtonClickHandler();
-//
-//			} else {
-//				$("#myModal").modal('show'); // rerender modal with errors
-//			}
-//		});
-		var minRange = -20;
-		var maxRange = 20;
-		$('#myForm').bootstrapValidator({
-			message: 'This value is not valid',
-			feedbackIcons: {
-				valid: 'glyphicon glyphicon-ok',
-				invalid: 'glyphicon glyphicon-remove',
-				validating: 'glyphicon glyphicon-refresh'
+$(document).ready(function () {
+	/** BOOTSTRAP VALIDATOR STUFF **/
+	var minRange = -256;
+	var maxRange = 256;
+	$('#myForm').bootstrapValidator({
+		feedbackIcons: {
+			valid: 'glyphicon glyphicon-ok',
+			invalid: 'glyphicon glyphicon-remove',
+			validating: 'glyphicon glyphicon-refresh'
+		},
+		fields: {
+			start1: {
+				message: 'This number is invalid',
+				validators: {
+					between: {
+						min: minRange,
+						max: maxRange,
+						message: 'Value must be between ' + minRange + ' and ' + maxRange
+					},
+					notEmpty: {
+						message: 'This start value is required and cannot be empty'
+					},
+					integer: {
+						message: 'This value is not an integer'
+					},
+					callback: {
+						message: "Range error",
+						callback: function (value, validator) {
+							return true;
+						}
+					}
+				}
 			},
-			fields: {
-				start1: {
-					message: 'This number is invalid',
-					validators: {
-						between: {
-							min: minRange,
-							max: maxRange,
-							message: 'Value must be between ' + minRange + ' and ' + maxRange
-						},
-						notEmpty: {
-							message: 'This start value is required and cannot be empty'
-						},
-						integer: {
-							message: 'This value is not an integer'
+			start2: {
+				message: 'This number is invalid',
+				validators: {
+					between: {
+						min: minRange,
+						max: maxRange,
+						message: 'Value must be between ' + minRange + ' and ' + maxRange
+					},
+					notEmpty: {
+						message: 'This start value is required and cannot be empty'
+					},
+					integer: {
+						message: 'This value is not an integer'
+					},
+					callback: {
+						message: "Range error",
+						callback: function (value, validator) {
+							return true;
 						}
 					}
-				},
-				start2: {
-					message: 'This number is invalid',
-					validators: {
-						between: {
-							min: minRange,
-							max: maxRange,
-							message: 'Value must be between ' + minRange + ' and ' + maxRange
-						},
-						notEmpty: {
-							message: 'This start value is required and cannot be empty'
-						},
-						integer: {
-							message: 'This value is not an integer'
+				}
+			},
+			end1: {
+				message: 'This number is invalid',
+				validators: {
+					between: {
+						min: minRange,
+						max: maxRange,
+						message: 'Value must be between ' + minRange + ' and ' + maxRange
+					},
+					notEmpty: {
+						message: 'This end value is required and cannot be empty'
+					},
+					integer: {
+						message: 'This value is not an integer'
+					},
+					callback: {
+						message: "Range error",
+						callback: function (value, validator) {
+							return true;
 						}
 					}
-				},
-				end1: {
-					message: 'This number is invalid',
-					validators: {
-						between: {
-							min: minRange,
-							max: maxRange,
-							message: 'Value must be between ' + minRange + ' and ' + maxRange
-						},
-						notEmpty: {
-							message: 'This end value is required and cannot be empty'
-						},
-						integer: {
-							message: 'This value is not an integer'
-						}
-					}
-				},
-				end2: {
-					message: 'This number is invalid',
-					validators: {
-						between: {
-							min: minRange,
-							max: maxRange,
-							message: 'Value must be between ' + minRange + ' and ' + maxRange
-						},
-						notEmpty: {
-							message: 'This end value is required and cannot be empty'
-						},
-						integer: {
-							message: 'This value is not an integer'
+				}
+			},
+			end2: {
+				message: 'This number is invalid',
+				validators: {
+					between: {
+						min: minRange,
+						max: maxRange,
+						message: 'Value must be between ' + minRange + ' and ' + maxRange
+					},
+					notEmpty: {
+						message: 'This end value is required and cannot be empty'
+					},
+					integer: {
+						message: 'This value is not an integer'
+					},
+					callback: {
+						message: "Range error",
+						callback: function (value, validator) {
+							return true;
 						}
 					}
 				}
 			}
-		})
-				  .on('success.', function(e, data) {
-					  if (data.bv.isValid()) {
-						  console.log("all fields valid");
-						  data.bv.disableSubmitButtons(false);
-					  }
-					  // e, data parameters are the same as in error.field.bv event handler
-					  // Despite that the field is valid, by default, the submit button will be disabled if all the following conditions meet
-					  // - The submit button is clicked
-					  // - The form is invalid
-				  });
+		}
 
+	})
+			// LISTENERS
+			// sucessful form validation and button pressed
+			.on('success.form.bv', function (e) {
+				console.log("calling additional validation");
+				if (validation()) { // preform number validation
+					AddTabButtonClickHandler(); // adds new tab with table
 
+				}
+
+			})
+			// every time a field is successfully updated
+			.on('success.field.bv', function (e, data) { // updates the global values dynamically
+				var value = $("input[name='" + data.field + "']").val();
+				if (data.field === "start1") {
+					startVal1 = value;
+				}
+				if (data.field === "start2") {
+					startVal2 = value;
+				}
+				if (data.field === "end1") {
+					endVal1 = value;
+				}
+				if (data.field === "end2") {
+					endVal2 = value;
+				}
+				data.bv.disableSubmitButtons(false);
+			});
+	// REMOVE ALL TABS
+	$("#removeTabs").on('click', function () {
+		$("[id^='span']").trigger('click'); // trigger click on x for each tab
 	});
 
+	/** TAB HANLDER STUFF **/
 	$("#myTabs").tabs();
-	// console.log( "parent id = " + $("#a").parent().attr( "id" ) ) ;
-
 	// the div containing the complete tabs structure
-	// var tabsdiv = $(this).parent().parent() ; 
 	var tabsdiv = $("#myTabs");
-
-// the list of tabs
+	// the list of tabs
 	var tabslist = $("#tab-row");
-
 	// set the number of the next tab to add
 	var nextTabNo = tabslist.find("li").length;
-
 	// this function is executed when an add-tab button is clicked
-	var AddTabButtonClickHandler = function() {
-		// console.log( "parent().parent() id = " + $(this).parent().parent().attr("id") ) ;
-		// console.log( $(this).parent().parent().find("ul li").length ) ;
-
+	var AddTabButtonClickHandler = function () {
 		// add content to the new tab
 		tabsdiv.append('<div id="' + String.fromCharCode(97 + nextTabNo) + '">' +
-				  renderTable() + '</div>');
-
+				renderTable() + '</div>');
 		// create a new tab
 		tabslist.append('<li><a href="#' + String.fromCharCode(97 + nextTabNo) + '">' +
-				  'Tab ' + (nextTabNo + 1) + ' <span id="span' + nextTabNo + '" class="remove-tab" aria-hidden="true">&times;</span></a></li>'); // do not remove span id
-
-		// add Add Tab and Remove Tab buttons to the new tab
-		// $("#" + String.fromCharCode( 97+ntabs ) ).append( 
-//		$("#" + String.fromCharCode(97 + nextTabNo)).append(
-//				  '<button class="add-tab">Add Tab</button> ' +
-//				  '<button class="remove-tab">Remove Tab</button>');
-
+				startVal1 + "-" + endVal1 + " by " + startVal2 + "-" + endVal2 + ' <span id="span' + nextTabNo + '" class="remove-tab" aria-hidden="true">&times;</span></a></li>'); // do not remove span id
 		// refresh the tab structure to make the newly added components appear
 		$("#myTabs").tabs("refresh");
-
 		// add click handler to all buttons with class add-tab
 		// note that this statement must be executed AFTER the tabs structure is refreshed
-		// $("#" + String.fromCharCode( 97+ntabs ) + " .add-tab").click( AddTabButtonClickHandler ) ;
-		// $("#" + String.fromCharCode( 97+ntabs ) + " .remove-tab").click( RemoveTabButtonClickHandler ) ;
-//		$("#" + String.fromCharCode(97 + nextTabNo) + " .add-tab").click(AddTabButtonClickHandler);
 		$("#span" + nextTabNo).click(RemoveTabButtonClickHandler);
-
 		tabsdiv.tabs('option', 'active', totalTabs); // set this tab to be focused
 		// increment number of next tab to add
 		nextTabNo++;
 		totalTabs++;
 	};
-
-// this function is executed when an add-tab button is clicked
-	var RemoveTabButtonClickHandler = function() {
-//		console.log("this: ");
-//		console.log($(this));
-
-		// remove tab 
-//		console.log($(this).parent().context.getAttribute("id").slice(4));
+	// this function is executed when the x is clicked on the tab
+	var RemoveTabButtonClickHandler = function () {
+		// get tab id #	
 		var id = Number($(this).parent().context.getAttribute("id").slice(4));
-
-//		$(this).remove();
+		// remove tab 
 		$(this).parent().parent().remove();
-
-//		// remove tab itself        
-//		var id = $(this).attr("id");
-//		// console.log( "need to remove tab with href = #" + id ) ;
+		// remove tab content 
 		var tabToRemove = String.fromCharCode(97 + id);
-//		// console.log( "tabToRemove = " + tabToRemove.html() ) ;
 		$("#" + tabToRemove).remove();
-
 		// refresh the tab structure to make the newly added components appear
 		$("#myTabs").tabs("refresh");
 		totalTabs--; // this tab has now been removed 
 		tabsdiv.tabs('option', 'active', totalTabs - 1); // set this tab to be focused (next tab down)
 	};
-
-
-//	// add the Add Tab button click handler to all All Tab buttons
-//	$(".add-tab").click(AddTabButtonClickHandler);
-//
-//	// add the Add Tab button click handler to all All Tab buttons
+	// add the Add Tab button click handler to all All Tab buttons
 	$(".remove-tab").click(RemoveTabButtonClickHandler);
 
-});
+	/**
+	 * This preforms my additional validation.
+	 * @param {type} event
+	 * @returns {Boolean}
+	 */
+	var validation = function (event) {
+		// preform validation here
+		resetForm(); // reset all errors fields and close error messages
 
-var validation = function(event) {
-	// preform validation here
-	resetForm(); // reset all errors fields and close error messages
+		// get the value from all fields
+		errorMessage = "ERROR: ";
+		var errorOccurred = false;
 
-	// get the value from all fields
-	startVal1 = parseInt($("#startVal1").val());
-	endVal1 = parseInt($("#endVal1").val());
-	startVal2 = parseInt($("#startVal2").val());
-	endVal2 = parseInt($("#endVal2").val());
-	errorMessage = "The red highlighted fields have errors: ";
-	var errorOccurred = false;
+//		console.log(startVal1 + " " + endVal1 + " " + startVal2 + " " + endVal2);
 
-	console.log(startVal1 + " " + endVal1 + " " + startVal2 + " " + endVal2);
-
-	// if one of the fields does not have a value
-	if (!$.isNumeric(startVal1)) {
-		$("#startVal1Group").addClass("has-error");
-		errorMessage += "Multiplier(start value) ";
-		errorOccurred = true;
-	}
-	if (!$.isNumeric(endVal1)) {
-		$("#endVal1Group").addClass("has-error");
-		errorMessage += "Multiplier(end value) ";
-		errorOccurred = true;
-	}
-	if (!$.isNumeric(startVal2)) {
-		$("#startVal2Group").addClass("has-error");
-		errorMessage += "Multiplicand(start value) ";
-		errorOccurred = true;
-	}
-	if (!$.isNumeric(endVal2)) {
-		$("#endVal2Group").addClass("has-error");
-		errorMessage += "Multiplicand(end value) ";
-		errorOccurred = true;
-	}
-
-	// if the start value is greater than the end value 
-	if (startVal1 > endVal1) {
-		$("#startVal1Group").addClass("has-error");
-		$("#endVal1Group").addClass("has-error");
-		errorMessage += "Multiplier(start value) must be smaller than its (end value) ";
-		errorOccurred = true;
-	}
-	if (startVal2 > endVal2) {
-		$("#startVal2Group").addClass("has-error");
-		$("#endVal2Group").addClass("has-error");
-		errorMessage += "Multiplicand(start value) must be smaller than its (end value) ";
-		errorOccurred = true;
-	}
-	// if an any field is not numeric
-	if (errorOccurred) {
-		$("#multForm-error").removeClass("hidden");
-		$("#multForm-errorMessage").html(errorMessage);
-		return false;
-	} else {
-		$("#myModal").modal('hide');
-		return true;
-		resetForm();
-	}
-
-};
-// resets all error conditions and closes error messages
-var resetForm = function() {
-	$("#multForm-error").addClass("hidden"); // hide alert message
-	$("#startVal1Group").removeClass("has-error");
-	$("#endVal1Group").removeClass("has-error");
-	$("#startVal2Group").removeClass("has-error");
-	$("#endVal2Group").removeClass("has-error");
-};
-
-// create and append multiplication table
-var renderTable = function() {
-	var tableString = "";
-	var tableHead = "";
-	var tableBody = "";
-	tableString += "<div class='table-responsive'>";
-	tableString += "<table class='table table-condensed table-striped'>";
-	tableString += "<thead>";
-	tableString += "<th>";
-	tableString += "</th>";
-	// TABLE HEAD
-	for (var i = startVal1; i <= endVal1; i++) {
-		tableHead += "<th>";
-		tableHead += i;
-		tableHead += "</th>";
-	}
-	tableString += tableHead;
-	tableString += "</thead>";
-
-	// TABLE BODY
-	for (var x = startVal2; x <= endVal2; x++) {
-		tableBody += "<tr>";
-		tableBody += "<td>";
-		tableBody += x;
-		tableBody += "</td>";
-
-		for (var y = startVal1; y <= endVal1; y++) {
-			tableBody += "<td>";
-			tableBody += (y * x);
-			tableBody += "</td>";
+		// if the start value is greater than the end value 
+		if (startVal1 > endVal1) {
+			$("#multiplier-label").addClass("text-danger");
+			errorMessage += "Multiplier (start value) must be smaller than its (end value) ";
+			$("#myForm").bootstrapValidator('updateStatus', 'start1', 'INVALID', 'callback');
+			$("#myForm").bootstrapValidator('updateStatus', 'end1', 'INVALID', 'callback');
+			errorOccurred = true;
 		}
-		tableBody += "</tr>";
-	}
-	tableString += tableBody;
-	tableString += "</table>";
-	tableString += "</div>";
+		if (startVal2 > endVal2) {
+			$("#multiplicand-label").addClass("text-danger");
+			errorMessage += "Multiplicand (start value) must be smaller than its (end value) ";
+			$("#myForm").bootstrapValidator('updateStatus', 'start2', 'INVALID', 'callback');
+			$("#myForm").bootstrapValidator('updateStatus', 'end2', 'INVALID', 'callback');
+			errorOccurred = true;
+		}
 
-	return tableString;
-//	$("#myTable").html(tableString);
-};
+		// show error messages if error occured
+		if (errorOccurred) {
+			if (errorMessage !== "ERROR: ") {
+				$("#multForm-error").removeClass("hidden"); // show alert message
+				$("#multForm-errorMessage").html(errorMessage);
+			}
+			return false;
+		} else {
+			resetForm();
+			resetValues();
+			return true;
+		}
+
+	};
+	// resets all error conditions and closes error messages
+	var resetForm = function () {
+		$('#myForm').data('bootstrapValidator').resetForm(); // reset bootstrap validator classes
+		$("#multiplicand-label").removeClass("text-danger");
+		$("#multiplier-label").removeClass("text-danger");
+		$("#multForm-error").addClass("hidden"); // hide alert message
+	};
+	var resetValues = function () {
+		// reset field values
+		$("#startVal1").val('');
+		$("#startVal2").val('');
+		$("#endVal1").val('');
+		$("#endVal2").val('');
+	};
+
+	// create and append multiplication table
+	var renderTable = function () {
+		var tableString = "";
+		var tableHead = "";
+		var tableBody = "";
+		tableString += "<div class='table-responsive'>";
+		tableString += "<table class='table table-condensed table-striped'>";
+		tableString += "<thead>";
+		tableString += "<th>";
+		tableString += "</th>";
+		// TABLE HEAD
+		for (var i = startVal1; i <= endVal1; i++) {
+			tableHead += "<th>";
+			tableHead += i;
+			tableHead += "</th>";
+		}
+		tableString += tableHead;
+		tableString += "</thead>";
+		// TABLE BODY
+		for (var x = startVal2; x <= endVal2; x++) {
+			tableBody += "<tr>";
+			tableBody += "<td>";
+			tableBody += x;
+			tableBody += "</td>";
+			for (var y = startVal1; y <= endVal1; y++) {
+				tableBody += "<td>";
+				tableBody += (y * x);
+				tableBody += "</td>";
+			}
+			tableBody += "</tr>";
+		}
+		tableString += tableBody;
+		tableString += "</table>";
+		tableString += "</div>";
+		return tableString;
+	};
+});
 
