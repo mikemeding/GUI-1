655fd81e57d600eaf4f89a1a20113bdb75c8918e
mikemeding <mikeymeding@gmail.com>
2014-12-03 12:32:12 -0500

    tabs are now a navbar. added auto scroll to the top method. added 1 album to mphoto. cleaned up css

--- RootSite/public_html/js/siteRoot.js
+++ RootSite/public_html/js/siteRoot.js
@@ -4,12 +4,13 @@
 //Created on: 09/09/2014
 //Course Info: 91.461 GUI programming 1 Jesse M. Heines 
 //
-$(document).ready(function() { // Triggered when DOM ready
-	// change hashcode to home page
-	var hash = "#home";
-	location.hash = hash;
-	// load that page
-	navigate();
+$(document).ready(function () {
+	// go to home by default on page load 
+	if (location.hash === "#home") {
+		navigate();
+	} else {
+		$("[href='#home']").trigger("click");
+	}
 });
 
 /**
@@ -19,7 +20,7 @@ function navigate() {
 	var hash = location.hash;
 	fragmentId = location.hash.substr(1); // strip off # from hash
 	// fetch content and load to tab
-	$(hash).load(fragmentId + "Info.html", function() {
+	$(hash).load(fragmentId + "Info.html", function () {
 		console.log("loaded " + hash + " tab data");
 		$("#tab-" + fragmentId).tab("show");
 	});
@@ -30,10 +31,17 @@ function navigate() {
 window.addEventListener("hashchange", navigate, false);
 
 // when a tab is clicked
-$('.nav-tabs a').click(function(e) {
+$('#mainTabs li a').click(function (e) {
 	e.preventDefault(); // this is uber important
 	location.hash = this.hash; // this triggers a hashchange event
 });
 
+// activate all tooltips on page
 $('[data-toggle="tooltip"]').tooltip({'placement': 'right'});
 
+// default start at top of page with slow scroll from:
+// http://stackoverflow.com/questions/1144805/how-do-i-scroll-to-the-top-of-the-page-with-jquery
+$("[data-toggle='tab']").click(function () {
+	$("html, body").animate({scrollTop: 0}, "slow");
+	return false;
+});
